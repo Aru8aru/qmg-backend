ee3997b6a5fe6b90340a3a58cd67f2555900936e
Аружан Аманова <aruzhan121@outlook.com>
2026-05-13 22:15:02 +0500

    update site

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,36 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
 dotenv.config();
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Backend works!')
-})
 
 app.use(cors());
 app.use(express.json());
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 app.post("/send-code", async (req, res) => {
   try {
     const { email, code } = req.body;
 
-    const data = await resend.emails.send({
-      from: "SCADA Security <onboarding@resend.dev>",
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
       to: email,
-      subject: "🔐 SCADA KZ-01 Security Code",
-      html: `
-        <div style="
-          background:#020817;
-          color:white;
-          padding:40px;
-          font-family:Arial;
-        ">
-          <h1 style="color:#00ffd5;">
-            SCADA KZ-01
-          </h1>
-
-          <p>Ваш код входа:</p>
-
-          <div style="
-            font-size:42px;
-            color:#00ffd5;
-            letter-spacing:8px;
-            font-weight:bold;
-          ">
-            ${code}
-          </div>
-
-          <p>Код действует 5 минут.</p>
-
-          <p style="color:#ffcc00">
-            Никому не сообщайте этот код.
-          </p>
-        </div>
-      `,
+      subject: "SCADA Security Code",
+      html: `<h1>Код: ${code}</h1>`,
     });
 
     res.json({
       success: true,
-      data,
     });
 
   } catch (err) {
@@ -71,4 +45,3 @@ app.post("/send-code", async (req, res) => {
 app.listen(process.env.PORT || 3001, () => {
   console.log("Server started");
 });
-
