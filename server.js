72d44802fb9252d22027f47160dfa46a62671fdd
Аружан Аманова <aruzhan121@outlook.com>
2026-05-13 19:22:33 +0500

    switch to nodemailer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+import nodemailer from "nodemailer";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Resend } from "resend";
 import mongoose from "mongoose";
 
 dotenv.config();
@@ -9,6 +9,13 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 // ─── MongoDB ──────────────────────────────────────────────────────────────────
 const connectDB = async () => {
@@ -38,7 +45,6 @@ const msgSchema = new mongoose.Schema({
 const Message = mongoose.models.Message || mongoose.model("Message", msgSchema);
 
 // ─── Resend ───────────────────────────────────────────────────────────────────
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 // ─── Routes ───────────────────────────────────────────────────────────────────
 app.get("/", (req, res) => {
@@ -49,26 +55,18 @@ app.get("/", (req, res) => {
 app.post("/send-code", async (req, res) => {
   try {
     const { email, code } = req.body;
-    if (!email || !code) {
-      return res.status(400).json({ error: "Email or code missing" });
-    }
-    const data = await resend.emails.send({
-      from: "SCADA Security <onboarding@resend.dev>",
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
       to: email,
-      subject: "🔐 SCADA KZ-01: Код двухфакторной аутентификации",
+      subject: "SCADA KZ-01 код",
       html: `
-        <div style="background:#020817;color:white;padding:40px;font-family:Arial;">
-          <h1 style="color:#00ffd5;">SCADA KZ-01</h1>
-          <p>Для входа в систему используйте код:</p>
-          <div style="font-size:42px;letter-spacing:8px;font-weight:bold;color:#00ffd5;margin:30px 0;">
-            ${code}
-          </div>
-          <p>Код действует 5 минут.</p>
-          <p style="color:#ffcc00;">Никому не сообщайте этот код.</p>
-        </div>
+        <h1>Ваш код:</h1>
+        <h2>${code}</h2>
       `,
     });
-    res.json({ success: true, data });
+
+    res.json({ success: true });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Email sending failed" });
